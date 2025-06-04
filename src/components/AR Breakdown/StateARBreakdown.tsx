3b06f34cc6ae5496c7ac3c9d90e352f25854a7ce
nneveu-cannabiz <nickole@cannabizcredit.com>
2025-06-04 16:03:35 -0400

    Reduce chart top margin

--- src/components/AR Breakdown/StateARBreakdown.tsx
+++ src/components/AR Breakdown/StateARBreakdown.tsx
@@ -204,7 +204,7 @@ const StateARBreakdown: React.FC<StateARBreakdownProps> = ({ selectedState, sele
               <ResponsiveContainer>
                 <BarChart 
                   data={chartData} 
-                  margin={{ top: 40, right: 25, left: 40, bottom: 5 }}
+                  margin={{ top: 20, right: 25, left: 40, bottom: 5 }}
                   barCategoryGap={20}
                 >
                   <CartesianGrid strokeDasharray="3 3" stroke="#f0f0f0" />
